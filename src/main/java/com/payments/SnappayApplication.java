02f4bf6f0df8e81fd7c65b32910ea3d75af3ddc0
Gauravpreet Singh <singhgauravarora@gmail.com>
2025-02-12 17:55:41 +0530

    Snappay project - added space

--- src/main/java/com/payments/SnappayApplication.java
+++ src/main/java/com/payments/SnappayApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SnappayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SnappayApplication.class, args);
+
     }
 }
